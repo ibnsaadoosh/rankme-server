76be10542e9582df8fc4439b140515614f7f99f6
Mohamed Saad <ibnsaadoosh@yahoo.com>
2021-07-22 03:19:28 +0200

    first version without the nlp model yet

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
         callback(null, dirPath)
     },
     filename: (req, file, callback) => {
-        callback(null, file.originalname + '-' + new Date().toUTCString())
+        callback(null, new Date().toUTCString() + '$' + file.originalname)
     }
 });
 
@@ -41,13 +41,13 @@ fileUpload.route('/')
     Jobs.create(req.body)
     .then((job) => {
         req.files.map((file) => {
-            job.resumes = job.resumes.concat({filename: file.filesname, path: file.path, percentage: Math.random(), jobId: job._id});
+            job.resumes = job.resumes.concat({filename: file.filename, path: file.path, percentage: Math.random(), jobId: job._id});
         });
         job.save()
         .then((job) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json(job);
+            res.json({sucess: "true", job: job});
         }, (err) => next(err))
         .catch((err) => next(err));
     }, (err) => next(err))
@@ -91,7 +91,9 @@ fileUpload.route('/:jobID')
     Jobs.findByIdAndUpdate(req.params.jobID,
                            {$set: req.body},
                            {new: true})
+    
     .then((job) => {
+        //nlp model(req.body.description, job.resumes)
         res.statusCode = 200,
         res.setHeader('Content-Type', 'application/json'),
         res.json(job);
@@ -107,9 +109,93 @@ fileUpload.route('/:jobID')
         });
         res.statusCode = 200,
         res.setHeader('Content-Type', 'application/json');
-        res.json(job);
+        res.json({sucess: "true"});
     }, (err) => next(err))
     .catch((err) => next(err));
+});
+
+fileUpload.route('/:jobID/resumes')
+
+.get((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /jobs' + req.params.jobID + '/resumes');
+})
+
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /jobs' + req.params.jobID + '/resumes');
+})
+
+.post(upload.array('filefield', 10000), (req, res, next) => {
+    //nlp_model
+    Jobs.findById(req.params.jobID)
+    .then((job) => {
+        req.files.map((file) => {
+            job.resumes = job.resumes.concat({filename: file.filesname, path: file.path, percentage: Math.random(), jobId: job._id});
+        });
+        
+        job.save()
+        .then((job) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(job);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+.delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /jobs' + req.params.jobID + '/resumes');
+})
+
+fileUpload.route('/:jobID/resumes/:resumeID')
+.get((req, res, next) => {
+    Jobs.findById(req.params.jobID)
+    .then((job) => {
+        resume = job.resumes.find(cv => cv._id == req.params.resumeID);
+        if(resume)
+        {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resume);
+        }
+        else
+        {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({sucess: "false", msg: "didn't find this resume"});
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported no /jobs/'+ req.params.jobID + '/resumes/' + req.params.resumeID);
+})
+
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported no /jobs/'+ req.params.jobID + '/resumes/' + req.params.resumeID);
+})
+
+.delete((req, res, next) => {
+    Jobs.findById(req.params.jobID)
+    .then((job) => {
+        resume = job.resumes.find(cv => cv._id == req.params.resumeID);
+        fsExtra.removeSync(resume.path);
+        job.resumes.remove(resume);
+        job.save()
+        .then((job) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(job);
+        }, (err) => next(err))
+        .catch((err) => next(err));        
+    })
+    .catch((err) => next(err))
 });
 
 module.exports = fileUpload;
